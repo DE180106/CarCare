67cc3d1a481e8b2f158a508826fa5c02e4d0f7c8
VuViet06 <vudinhviet162@gmail.com>
2025-07-12 06:41:32 +0700

    Booking, map, chat

--- carcare/src/App.js
+++ carcare/src/App.js
@@ -1,29 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import BookingForm from "./services";
+import StoreSystem from "./garages";
+import Contact from "./contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/services" />} />
+      <Route path="/services" element={<BookingForm />} />
+      <Route path="/garages" element={<StoreSystem />} />
+      <Route path="/contact/:userId/:garageId" element={<Contact />} /> {/* Dynamic route */}
+    </Routes>
   );
 }
 
 export default App;
-
-// asdfghjkl;'
-// hihihi
-
